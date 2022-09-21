361269ba5df33fb879f01b2d465cdfe2669246bd
alexbellairs <alexanderbellairs@gmail.com>
2022-09-21 18:37:15 +0100

    logos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import "../CSS/navbar.css";
+import premlogo from "../images/PremLogo.png";
 
 const Navbar = () => {
   return (
     <div className="nav">
+      <img className="pl" src={premlogo} alt="Prem Logo" />
       <Link to="/">Home</Link>
+      <img className="pl" src={premlogo} alt="Prem Logo" />
     </div>
   );
 };
--- src/pages/Jason.js
+++ src/pages/Jason.js
@@ -1,30 +1,110 @@
 import "../CSS/pages.css";
+import afcb from "../images/AFC_Bournemouth.png";
+import afc from "../images/Arsenal.png";
+import avfc from "../images/Aston_Villa.png";
+import bfc from "../images/brentford.png";
+import bha from "../images/Brighton.png";
+import cfc from "../images/Chelsea.png";
+import cpfc from "../images/Crystal_Palace.png";
+import efc from "../images/Everton.png";
+import ffc from "../images/Fulham.png";
+import lufc from "../images/Leeds.png";
+import lcfc from "../images/Leicester.png";
+import lfc from "../images/Liverpool.png";
+import mcfc from "../images/MCFC.png";
+import mufc from "../images/MUFC.png";
+import nufc from "../images/Newcastle.png";
+import nffc from "../images/Nottingham.png";
+import sfc from "../images/Southampton.png";
+import thfc from "../images/Tottenham.png";
+import whufc from "../images/West_Ham.png";
+import wwfc from "../images/Wolves.png";
 
 const Jason = () => {
   return (
     <div className="leag">
       <h3>Jason Predicted Table</h3>
       <ol className="tab">
-        <li>Manchester City</li>
-        <li>Liverpool</li>
-        <li>Tottenham Hotspurs</li>
-        <li>Manchester United</li>
-        <li>Chelsea</li>
-        <li>Arsenal</li>
-        <li>West Ham United</li>
-        <li>Newcastle United</li>
-        <li>Wolverhampton Wanderers</li>
-        <li>Aston Villa</li>
-        <li>Leicester City</li>
-        <li>Brighton & Hove Albion</li>
-        <li>Crystal Palace</li>
-        <li>Brentford</li>
-        <li>Leeds United</li>
-        <li>Everton</li>
-        <li>Southampton</li>
-        <li>Bournemouth</li>
-        <li>Nottingham Forrest</li>
-        <li>Fulham</li>
+        <li>
+          <img src={mcfc} alt="MCFC" />
+          Manchester City
+        </li>
+        <li>
+          <img src={lfc} alt="LFC" />
+          Liverpool
+        </li>
+        <li>
+          <img src={thfc} alt="THFC" />
+          Tottenham Hotspurs
+        </li>
+        <li>
+          <img src={mufc} alt="MUFC" />
+          Manchester United
+        </li>
+        <li>
+          <img src={cfc} alt="CFC" />
+          Chelsea
+        </li>
+        <li>
+          <img src={afc} alt="AFC" />
+          Arsenal
+        </li>
+        <li>
+          <img src={whufc} alt="WHUFC" />
+          West Ham United
+        </li>
+        <li>
+          <img src={nufc} alt="NUFC" />
+          Newcastle United
+        </li>
+        <li>
+          <img src={wwfc} alt="WWFC" />
+          Wolverhampton Wanderers
+        </li>
+        <li>
+          <img src={avfc} alt="AVFC" />
+          Aston Villa
+        </li>
+        <li>
+          <img src={lcfc} alt="LCFC" />
+          Leicester City
+        </li>
+        <li>
+          <img src={bha} alt="BHAFC" />
+          Brighton & Hove Albion
+        </li>
+        <li>
+          <img src={cpfc} alt="CPFC" />
+          Crystal Palace
+        </li>
+        <li>
+          <img src={bfc} alt="BFC" />
+          Brentford
+        </li>
+        <li>
+          <img src={lufc} alt="LUFC" />
+          Leeds United
+        </li>
+        <li>
+          <img src={efc} alt="EFC" />
+          Everton
+        </li>
+        <li>
+          <img src={sfc} alt="SFC" />
+          Southampton
+        </li>
+        <li>
+          <img src={afcb} alt="AFCB" />
+          Bournemouth
+        </li>
+        <li>
+          <img src={nffc} alt="NFFC" />
+          Nottingham Forrest
+        </li>
+        <li>
+          <img src={ffc} alt="FFC" />
+          Fulham
+        </li>
       </ol>
     </div>
   );
