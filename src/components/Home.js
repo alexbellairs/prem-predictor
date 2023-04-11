0942c8344272d6368ad38e60922b2f03b9b428a4
alexbellairs <alexanderbellairs@gmail.com>
2023-04-11 11:37:50 +0100

    week27

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,73 +17,73 @@ const Home = () => {
           <tr className="first">
             <td>1.</td>
             <td>
-              <Link to="/Wilko">Wilko</Link>
+              <Link to="/Paula">Paula Bridge</Link>
             </td>
-            <td>66</td>
-            <td>+1</td>
-            <td>0</td>
+            <td>74</td>
+            <td>+3</td>
+            <td>3</td>
           </tr>
           <tr className="second">
             <td>2.</td>
             <td>
-              <Link to="/Adam">Adam Wilkinson</Link>
+              <Link to="/JoeG">Joe Godfrey</Link>
             </td>
-            <td>68</td>
-            <td>+2</td>
-            <td>2</td>
+            <td>76</td>
+            <td>+5</td>
+            <td>3</td>
           </tr>
           <tr>
             <td>3.</td>
             <td>
               <Link to="/Steve">Steve Lee</Link>
             </td>
-            <td>68</td>
-            <td>-2</td>
+            <td>76</td>
             <td>0</td>
+            <td>2</td>
           </tr>
           <tr>
             <td>4.</td>
             <td>
-              <Link to="/Paula">Paula Bridge</Link>
+              <Link to="/Wilko">Wilko</Link>
             </td>
-            <td>70</td>
-            <td>+1</td>
-            <td>1</td>
+            <td>76</td>
+            <td>-3</td>
+            <td>2</td>
           </tr>
           <tr>
             <td>5.</td>
             <td>
-              <Link to="/Paul">Paul Gunning</Link>
+              <Link to="/Adam">Adam Wilkinson</Link>
             </td>
-            <td>70</td>
-            <td>+2</td>
-            <td>0</td>
+            <td>76</td>
+            <td>-3</td>
+            <td>1</td>
           </tr>
           <tr>
             <td>6.</td>
             <td>
-              <Link to="/Springer">Tim Springer</Link>
+              <Link to="/Jason">Jason</Link>
             </td>
-            <td>72</td>
-            <td>-3</td>
-            <td>4</td>
+            <td>78</td>
+            <td>+4</td>
+            <td>1</td>
           </tr>
           <tr>
             <td>7.</td>
             <td>
-              <Link to="/JoeG">Joe Godfrey</Link>
+              <Link to="/Nick">Nick Cook</Link>
             </td>
-            <td>72</td>
-            <td>+1</td>
-            <td>0</td>
+            <td>80</td>
+            <td>+5</td>
+            <td>3</td>
           </tr>
           <tr>
             <td>8.</td>
             <td>
-              <Link to="/Sam">Sam Hazeldine</Link>
+              <Link to="/Beth">Beth Springer</Link>
             </td>
-            <td>74</td>
-            <td>+7</td>
+            <td>80</td>
+            <td>+5</td>
             <td>2</td>
           </tr>
           <tr>
@@ -91,179 +91,179 @@ const Home = () => {
             <td>
               <Link to="/DaveT">Dave Tracy</Link>
             </td>
-            <td>74</td>
-            <td>+4</td>
-            <td>1</td>
+            <td>80</td>
+            <td>0</td>
+            <td>2</td>
           </tr>
           <tr>
             <td>10.</td>
             <td>
-              <Link to="/Jason">Jason</Link>
+              <Link to="/Paul">Paul Gunning</Link>
             </td>
-            <td>74</td>
-            <td>-1</td>
-            <td>1</td>
+            <td>82</td>
+            <td>-5</td>
+            <td>2</td>
           </tr>
           <tr>
             <td>11.</td>
             <td>
-              <Link to="/Tony">Tony</Link>
+              <Link to="/Bennett">Mike Bennett</Link>
             </td>
-            <td>74</td>
-            <td>+6</td>
+            <td>82</td>
+            <td>+13</td>
             <td>1</td>
           </tr>
           <tr>
             <td>12.</td>
             <td>
-              <Link to="/Nick">Nick Cook</Link>
+              <Link to="/Springer">Tim Springer</Link>
             </td>
-            <td>76</td>
-            <td>-2</td>
-            <td>2</td>
+            <td>82</td>
+            <td>-6</td>
+            <td>1</td>
           </tr>
           <tr>
             <td>13.</td>
             <td>
-              <Link to="/Beth">Beth Springer</Link>
+              <Link to="/Tony">Tony</Link>
             </td>
-            <td>76</td>
-            <td>-1</td>
+            <td>84</td>
+            <td>-2</td>
             <td>1</td>
           </tr>
           <tr className="bogey">
             <td>14.</td>
             <td>
-              <Link to="/Rick">Rick</Link>
+              <Link to="/EvertonDave">Everton Dave</Link>
             </td>
-            <td>78</td>
-            <td>+2</td>
-            <td>2</td>
+            <td>86</td>
+            <td>+5</td>
+            <td>3</td>
           </tr>
           <tr>
             <td>15.</td>
             <td>
-              <Link to="/Roy">Roy</Link>
+              <Link to="/Billy">Billy Sullivan</Link>
             </td>
-            <td>78</td>
-            <td>+7</td>
+            <td>86</td>
+            <td>+2</td>
             <td>2</td>
           </tr>
           <tr>
             <td>16.</td>
             <td>
-              <Link to="/Scott">Scott</Link>
+              <Link to="/Rick">Rick</Link>
             </td>
-            <td>78</td>
-            <td>+8</td>
-            <td>0</td>
+            <td>86</td>
+            <td>-2</td>
+            <td>2</td>
           </tr>
           <tr>
             <td>17.</td>
             <td>
-              <Link to="/Billy">Billy Sullivan</Link>
+              <Link to="/George">George Springer</Link>
             </td>
-            <td>80</td>
-            <td>+1</td>
+            <td>86</td>
+            <td>+5</td>
             <td>1</td>
           </tr>
           <tr>
             <td>18.</td>
             <td>
-              <Link to="/JohnB">John Bellairs</Link>
+              <Link to="/DaveH">Dave Healy</Link>
             </td>
-            <td>80</td>
-            <td>-12</td>
+            <td>86</td>
+            <td>+3</td>
             <td>0</td>
           </tr>
           <tr>
             <td>19.</td>
             <td>
-              <Link to="/EvertonDave">Everton Dave</Link>
+              <Link to="/Scott">Scott</Link>
             </td>
-            <td>82</td>
-            <td>+2</td>
-            <td>3</td>
+            <td>88</td>
+            <td>-3</td>
+            <td>4</td>
           </tr>
           <tr>
             <td>20.</td>
             <td>
-              <Link to="/Sullivan">Tim Sullivan</Link>
+              <Link to="/Roy">Roy</Link>
             </td>
-            <td>82</td>
-            <td>-1</td>
-            <td>3</td>
+            <td>88</td>
+            <td>-5</td>
+            <td>0</td>
           </tr>
           <tr>
             <td>21.</td>
             <td>
-              <Link to="/DaveH">Dave Healy</Link>
+              <Link to="/Sam">Sam Hazeldine</Link>
             </td>
-            <td>82</td>
-            <td>+2</td>
-            <td>2</td>
+            <td>88</td>
+            <td>-13</td>
+            <td>0</td>
           </tr>
           <tr>
             <td>22.</td>
             <td>
-              <Link to="/George">George Springer</Link>
+              <Link to="/Sullivan">Tim Sullivan</Link>
             </td>
-            <td>82</td>
-            <td>-8</td>
-            <td>2</td>
+            <td>88</td>
+            <td>-2</td>
+            <td>0</td>
           </tr>
           <tr>
             <td>23.</td>
             <td>
               <Link to="/Chris">Chris Markham</Link>
             </td>
-            <td>84</td>
-            <td>+3</td>
-            <td>3</td>
+            <td>90</td>
+            <td>0</td>
+            <td>2</td>
           </tr>
           <tr>
             <td>24.</td>
             <td>
-              <Link to="/Bennett">Mike Bennett</Link>
+              <Link to="/JohnB">John Bellairs</Link>
             </td>
-            <td>84</td>
-            <td>-13</td>
-            <td>2</td>
+            <td>90</td>
+            <td>-6</td>
+            <td>1</td>
           </tr>
           <tr>
             <td>25.</td>
             <td>
               <Link to="/Joe">Joe Gunning</Link>
             </td>
-            <td>84</td>
-            <td>+3</td>
-            <td>1</td>
+            <td>94</td>
+            <td>0</td>
+            <td>2</td>
           </tr>
           <tr>
             <td>26.</td>
             <td>
-              <Link to="/Al">Al Markham</Link>
+              <Link to="/Lewis">Lewis Markham</Link>
             </td>
-            <td>86</td>
-            <td>-6</td>
-            <td>3</td>
+            <td>94</td>
+            <td>+1</td>
+            <td>2</td>
           </tr>
           <tr>
             <td>27.</td>
             <td>
-              <Link to="/Lewis">Lewis Markham</Link>
+              <Link to="/DaveM">Dave Moody</Link>
             </td>
-            <td>86</td>
-            <td>0</td>
+            <td>96</td>
+            <td>+1</td>
             <td>2</td>
           </tr>
           <tr className="last">
             <td>28.</td>
             <td>
-              <Link to="/DaveM">Dave Moody</Link>
+              <Link to="/Al">Al Markham</Link>
             </td>
-            <td>86</td>
-            <td>-3</td>
+            <td>96</td>
+            <td>-2</td>
             <td>1</td>
           </tr>
         </table>
